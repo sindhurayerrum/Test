fcb77405105a7d0159aedcc7fd85f90f6b40c88f
Venkat <chaitra.infanion@gmail.com>
2017-04-27 19:07:57 +0530

    My second commit

--- Testdemo/src/demopackage/demogitclass.java
+++ Testdemo/src/demopackage/demogitclass.java
@@ -5,6 +5,9 @@ public class demogitclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("my first commit");
+
+      System.out.println("my second commit");
+      
 	}
 
 }
