c603f6f5cb2a578daaedbc06eb0315d285a79be4
Venkat <chaitra.infanion@gmail.com>
2017-04-27 17:36:58 +0530

    My second commit message

--- Demotest/src/Demosgit/demo1.java
+++ Demotest/src/Demosgit/demo1.java
@@ -5,6 +5,8 @@ public class demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My first commit..");
+		System.out.println("My second commit..");
+		
 
 	}
 
