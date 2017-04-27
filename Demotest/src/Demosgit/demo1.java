424599416ebf12fb5b498d39faf405b709dd8d47
Venkat <chaitra.infanion@gmail.com>
2017-04-27 17:33:53 +0530

    My first commit message

--- Demotest/src/Demosgit/demo1.java
+++ Demotest/src/Demosgit/demo1.java
@@ -4,7 +4,7 @@ public class demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("My first commit");
+		System.out.println("My first commit..");
 
 	}
 
